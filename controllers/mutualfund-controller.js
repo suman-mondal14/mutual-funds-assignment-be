bd7b413f55818fdad2c9d072f1b462b1df69c74c
suman-mondal14 <sumanmondal142003@gmail.com>
2025-06-28 20:23:14 +0530

    delete mutual funds api integrated

--- controllers/mutualfund-controller.js
+++ controllers/mutualfund-controller.js
@@ -37,3 +37,21 @@ export const getFundsByUser = async (req, res) => {
     res.status(500).json({ message: "Internal server error." });
   }
 };
+
+//delete mutual funds
+export const deleteFund = async (req, res) => {
+  try {
+    const { userId, schemeCode } = req.params;
+
+    const deleted = await MutualFund.findOneAndDelete({ userId, schemeCode });
+
+    if (!deleted) {
+      return res.status(404).json({ message: "Fund not found." });
+    }
+
+    res.status(200).json({ message: "Fund deleted successfully." });
+  } catch (error) {
+    console.error("Delete fund error:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
--- router/mutualfund-router.js
+++ router/mutualfund-router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   saveFund,
   getFundsByUser,
+  deleteFund,
 } from "../controllers/mutualfund-controller.js";
 
 const router = Router();
@@ -11,5 +12,8 @@ router.post("/save-mutual-fund", saveFund);
 
 // Get all saved funds by user ID
 router.get("/get-mutual-funds/:userId", getFundsByUser);
+
+//delete mutual funds
+router.delete("/delete-mutual-fund/:userId/:schemeCode", deleteFund);
 
 export default router;
